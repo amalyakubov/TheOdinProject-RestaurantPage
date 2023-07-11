2f07c41bcaaaddd49a73ffa9d8f2e493b0043873
amalyakubov <amal.yakubov7777@gmail.com>
2023-07-11 11:40:50 +0200

    Write a part of basic webpage loading functions
    Research the bug, where the image doesn't load

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import logo from './img/logo.png'
+
+function appendToDivWithText(element, text) {
+    let content = document.getElementById('content');
+    let div = document.createElement('div');
+    let elementToAppend = document.createElement(`${element}`);
+    elementToAppend.innerHTML = text;
+    div.appendChild(elementToAppend);
+    return div;
+}
+
+function logoAppend() {
+    let content = document.getElementById('content');
+    let logoToAppend = new Image();
+    logoToAppend.src = logo;
+    content.appendChild(logoToAppend);
+}
+
+function appendToHeader(element) {
+    let header = document.createElement('div');
+    header.id = 'header';
+    header.appendChild(element);
+    document.getElementById('content').appendChild(header);
+}
+
+function pageLoad(text) {
+    let content = document.getElementById('content');
+    let welcomeText = appendToDivWithText('p', text);
+    return appendToHeader(content.appendChild(welcomeText));
+}
+
+logoAppend();
+pageLoad('It is the fucking header!');
+
+alert('Welcome to Los Pollos Hermanos. Glad to see you here!');
